6197d5db2dba4ceb380f3a37f657eac96714fe5e
root <root@dmx.(none)>
2014-01-05 20:58:47 +0100

    updated onchange functionality

--- 03/public/script.js
+++ 03/public/script.js
@@ -1,13 +1,35 @@
 
 function FormController($scope, $http) {
   $scope.rooms = [
-    '101','102','201','202'
+    { id:'101', desc:'1 / 3+1, sociální zařízení, TV, lednice'},
+    { id:'102', desc:'1 / 2+1, sociální zařízení, TV, lednice'},
+    { id:'201', desc:'2 / 3+1, sociální zařízení, TV, lednice'},
+    { id:'202', desc:'2 / 2+1, sociální zařízení, TV'}
    ];
 
-  $scope.items = [ { room: '101', adults: 2, children: 1 } ];
+  $scope.items = [{ 
+    acc : { adults: 0, children: 0, extrabed: false, babybeds:0 }, 
+    food: { adults: 0, children: 0},
+    accPrice: 0, foodPrice: 0 
+  }];
+  $scope.totalPrice = 0;
 
   $scope.addRoom = function() {
-    $scope.items.push({ adults: 0, children: 0 });
+    $scope.items.push({ 
+      acc : { adults: 0, children: 0, extrabed: false, babybeds:0 }, 
+      food: { adults: 0, children: 0},
+      accPrice: 0, foodPrice: 0
+    });
+  };
+
+  $scope.change = function(item) {
+    item.accPrice = item.acc.adults * 100 + item.acc.children * 50;
+    item.foodPrice = item.food.adults * 10 + item.food.children * 5;
+    var total = 0;
+    for (i in $scope.items) {
+      total = total + $scope.items[i].accPrice + $scope.items[i].foodPrice;
+    }
+    $scope.totalPrice = total;
   };
 
   $scope.send = function() {
