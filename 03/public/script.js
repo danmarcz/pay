de300b33b327ca629a4b2de5072cd3e4efc4bd71
root <root@dmx.(none)>
2014-01-08 23:15:32 +0100

    updated calc/refresh functionality

--- 03/public/script.js
+++ 03/public/script.js
@@ -1,30 +1,78 @@
 
 function FormController($scope, $http) {
+
   $scope.rooms = [
-    { id:'101', desc:'1 / 3+1, sociální zařízení, TV, lednice'},
-    { id:'102', desc:'1 / 2+1, sociální zařízení, TV, lednice'},
-    { id:'201', desc:'2 / 3+1, sociální zařízení, TV, lednice'},
-    { id:'202', desc:'2 / 2+1, sociální zařízení, TV'}
+    { id:'101', desc:'1 / 3+1, soc. zařízení, TV, lednice', adultPrices: [420, 660, 900], extrabedPrice:225, childrenPrice: 200 },
+    { id:'102', desc:'1 / 3+1, soc. zařízení, TV, lednice', adultPrices: [420, 660, 900], extrabedPrice:225, childrenPrice: 200 },
+    { id:'103', desc:'1 / 3+1, soc. zařízení, TV, lednice', adultPrices: [420, 660, 900], extrabedPrice:225, childrenPrice: 200 },
+    { id:'104', desc:'1 / 3+1, soc. zařízení, TV, lednice', adultPrices: [420, 660, 900], extrabedPrice:225, childrenPrice: 200 },
+    { id:'105', desc:'1 / 3+1, soc. zařízení, TV, lednice', adultPrices: [420, 660, 900], extrabedPrice:225, childrenPrice: 200 },
+    { id:'106', desc:'1 / 3+1, soc. zařízení, TV, lednice', adultPrices: [420, 660, 900], extrabedPrice:225, childrenPrice: 200 },
    ];
+
+  $scope.foodTypes = [
+    { code:'N', name:'Bez stravy', adultPrice: 0, childPrice: 0 },
+    { code:'S', name:'Snídaně', adultPrice: 60, childPrice: 40 },
+    { code:'P', name:'Polopenze', adultPrice: 170, childPrice: 110 },
+    { code:'PP', name:'Plná penze', adultPrice: 270, childPrice: 180 }
+   ];
+
+  $scope.babybedPrice = 80;
 
   $scope.items = [{ 
     acc : { adults: 0, children: 0, extrabed: false, babybeds:0 }, 
     food: { adults: 0, children: 0},
-    accPrice: 0, foodPrice: 0 
+    accPrice: 0, foodPrice: 0,
+    room: $scope.rooms[0] 
   }];
+
   $scope.totalPrice = 0;
+  $scope.days = 1;
+  $scope.foodType = $scope.foodTypes[2];
 
   $scope.addRoom = function() {
     $scope.items.push({ 
       acc : { adults: 0, children: 0, extrabed: false, babybeds:0 }, 
       food: { adults: 0, children: 0},
-      accPrice: 0, foodPrice: 0
+      accPrice: 0, foodPrice: 0,
+      room: $scope.rooms[0]
     });
   };
 
+  $scope.changeAll = function() {
+    $scope.items.forEach(function traverse(element, index, array) {
+      $scope.change(element);
+    });
+  }
+
   $scope.change = function(item) {
-    item.accPrice = item.acc.adults * 100 + item.acc.children * 50;
-    item.foodPrice = item.food.adults * 10 + item.food.children * 5;
+    item.accPrice = 0;
+    if (item.acc.adults > 0 ) {
+      item.accPrice += item.acc.adults * item.room.adultPrices[item.acc.adults -1];
+    }
+    if (item.acc.children > 0) {
+      item.accPrice += item.acc.children * item.room.childrenPrice;
+    }
+    if (item.acc.extrabed == true) {
+      item.accPrice += item.room.extrabedPrice;
+    }
+    if (item.acc.babybeds > 0) {
+      item.accPrice += item.acc.babybeds * $scope.babybedPrice;
+    }
+
+    item.accPrice = item.accPrice * $scope.days;
+    // if ($scope.days == 1) {
+    //   item.accPrice = item.accPrice * 1.1;
+    // }
+    // if ($scope.days > 2) {
+    //   item.accPrice = item.accPrice * 0.9;
+    // }
+
+    item.foodPrice = item.food.adults * $scope.foodType.adultPrice 
+    + item.food.children * $scope.foodType.childPrice;
+
+    item.foodPrice = item.foodPrice * $scope.days;
+
     var total = 0;
     for (i in $scope.items) {
       total = total + $scope.items[i].accPrice + $scope.items[i].foodPrice;
